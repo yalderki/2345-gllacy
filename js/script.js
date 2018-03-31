2c1dfda7e564c231f6b63c4852168fdc1b09df34
Tatyana <tatjanayagutjeva@gmail.com>
2018-04-01 01:31:11 +0300

    Правит js

--- js/script.js
+++ js/script.js
@@ -1,52 +1,52 @@
-	var link = document.querySelector(".feedback-button");
-	var popup = document.querySelector(".modal-feedback");
-	var close = popup.querySelector(".modal-close");
-	var form = popup.querySelector(".feedback-form");
-	var feedbackname = popup.querySelector("[name=feedbackname]");
-	var feedbackemail = popup.querySelector("[name=feedbackemail]");
-	var feedbackcomment = popup.querySelector("[name=feedbackcomment]");
-	var isStorageSupport = true;
-	var storage = "";
-	  
-	try {
-	  storage = localStorage.getItem("feedbackname");
-	} catch (err) {
-	  isStorageSupport = false;
+var link = document.querySelector(".feedback-button");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector(".feedback-form");
+var feedbackname = popup.querySelector("[name=feedbackname]");
+var feedbackemail = popup.querySelector("[name=feedbackemail]");
+var feedbackcomment = popup.querySelector("[name=feedbackcomment]");
+var isStorageSupport = true;
+var storage = "";
+  
+try {
+  storage = localStorage.getItem("feedbackname");
+} catch (err) {
+  isStorageSupport = false;
+}
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	if (storage) {
+		feedbackname.value = storage;
+		feedbackemail.focus();
+	} else {
+		feedbackname.focus();
 	}
-	link.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		popup.classList.add("modal-show");
-		if (storage) {
-			feedbackname.value = storage;
-			feedbackemail.focus();
+});
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
+form.addEventListener("submit", function (evt) {
+	if (!feedbackname.value || !feedbackemail.value || !feedbackcomment.value) {
+			evt.preventDefault();
+			popup.classList.remove("modal-error");
+			popup.offsetWidth = popup.offsetWidth;
+			popup.classList.add("modal-error");
+ 			//console.log("Нужно ввести имя, почту и сообщение");
 		} else {
-			feedbackname.focus();
+		if (isStorageSupport) {
+			localStorage.setItem("feedbackname", feedbackname.value);
 		}
-	});
-	close.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		popup.classList.remove("modal-show");
-		popup.classList.remove("modal-error");
-	});
-	form.addEventListener("submit", function (evt) {
-		if (!feedbackname.value || !feedbackemail.value || !feedbackcomment.value) {
-   			evt.preventDefault();
-   			popup.classList.remove("modal-error");
-   			popup.offsetWidth = popup.offsetWidth;
-   			popup.classList.add("modal-error");
-    			//console.log("Нужно ввести имя, почту и сообщение");
-  			} else {
-  				if (isStorageSupport) {
-  					localStorage.setItem("feedbackname", feedbackname.value);
-  				}
-  			}
-	});
-	window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
-      }
-    }
-  });
+	}
+});
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+    	evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+       popup.classList.remove("modal-show");
+       popup.classList.remove("modal-error");
+    	}
+	}
+});
